e88e1cf1d4bc53b7ecd293f50ab7e72ea68c38a3
tusharpatekar <tusharpatekar45@gmail.com>
2024-03-23 17:25:25 +0530

    Last commit of project

--- src/main/java/com/dailycodebuffer/Employee/controller/EmployeeController.java
+++ src/main/java/com/dailycodebuffer/Employee/controller/EmployeeController.java
@@ -3,9 +3,12 @@ package com.dailycodebuffer.Employee.controller;
 
 import com.dailycodebuffer.Employee.model.Employee;
 import com.dailycodebuffer.Employee.services.EmployeeService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -29,6 +32,33 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
+
+    }
+
+    @DeleteMapping("/employees/{id}")
+
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
+        boolean deleted = false;
+        deleted = employeeService.deleteEmployee(id);
+        Map<String,Boolean> response = new HashMap<>();
+        response.put("deleted",deleted);
+        return ResponseEntity.ok(response);
+
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+
+        Employee employee = null;
+        employee = employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(employee);
+
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee){
+        employee = employeeService.updateEmployee(id,employee);
+        return ResponseEntity.ok(employee);
 
     }
 }
